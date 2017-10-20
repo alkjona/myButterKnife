1f4c40ab9f4f2db94c3f2e343cec1427217424b3
admin <admin@iMac-de-admin-3.local>
2017-10-20 18:16:00 -0500

    Initial commit

---
+++ app/src/main/java/com/code3me/mybutterknife/MainActivity.java
@@ -0,0 +1,28 @@
+package com.code3me.mybutterknife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.textView) TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.button)
+    public void buttonPressed(){
+        textView.setText("Hellow Butter!!!");
+    }
+}
